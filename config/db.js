031086d54252681621c5642b9b459bf450c8f150
Cameron Perrin <perrin.cameron@gmail.com>
2016-09-09 11:30:13 +0100

    Updated mongodb_uri

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 dbURIs = {
   test: "mongodb://localhost/choosite-test",
   development: "mongodb://localhost/choosite-app",
-  production: process.env.MONGOLAB_URI || "mongodb://localhost/choosite-app"
+  production: process.env.MONGODB_URI || "mongodb://localhost/choosite-app"
 }
 
 module.exports = function(env) {
