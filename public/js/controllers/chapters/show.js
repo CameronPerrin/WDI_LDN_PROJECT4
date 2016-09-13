7e9cb4851387ca952cae6e307029bb0ac67461a1
Cameron Perrin <perrin.cameron@gmail.com>
2016-09-13 11:45:57 +0100

    only creators can now edit/delete their post

--- public/js/controllers/chapters/show.js
+++ public/js/controllers/chapters/show.js
@@ -2,14 +2,25 @@ angular
   .module('Choosite')
   .controller("ChaptersShowController", ChaptersShowController);
 
-ChaptersShowController.$inject = ["Chapter", "$state"];
-function ChaptersShowController(Chapter, $state) {
+ChaptersShowController.$inject = ["Chapter", "$state","TokenService"];
+function ChaptersShowController(Chapter, $state, TokenService) {
   this.selected = Chapter.get($state.params);
 
   this.delete = function() {
     this.selected.$delete(function() {
       $state.go("chaptersIndex");
     });
+  }
+
+
+  this.currentUser = TokenService.decodeToken();
+
+  this.whoOwns = function whoOwns() {
+    if(this.currentUser._id === this.selected.owner){
+      return true;
+    } else {
+      return false;
+    }
   }
 
   // this.optionOne = Chapter.get($stateparams);
